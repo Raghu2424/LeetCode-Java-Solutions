56d9e6ab8f697cd70eec73a497c220d7d44b1663
Raghavendra <raghavendrakshetri@gmail.com>
2026-08-25 22:39:36 +0530

    Add Day 21 solution: Smallest Missing Multiple of K

---
+++ Day-21-Smallest-Missing-Multiple-of-K/Solution.java
@@ -0,0 +1,20 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int missingMultiple(int[] nums, int k) {
+        Set<Integer> seen = new HashSet<>();
+
+        for (int num : nums) {
+            seen.add(num);
+        }
+
+        int multiple = k;
+
+        while (seen.contains(multiple)) {
+            multiple += k;
+        }
+
+        return multiple;
+    }
+}
