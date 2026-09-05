7ec1daddf230c3033885cae340dd20466d2e8755
Raghavendra <raghavendrakshetri@gmail.com>
2026-09-05 19:18:12 +0530

    Add Day 30 solution: First Stable Index

---
+++ Day-30-First-Stable-Index/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+        int n = nums.length;
+        int[] suffMin = new int[n];
+
+        suffMin[n - 1] = nums[n - 1];
+
+        for (int i = n - 2; i >= 0; i--) {
+            suffMin[i] = Math.min(nums[i], suffMin[i + 1]);
+        }
+
+        int max = nums[0];
+
+        for (int i = 0; i < n; i++) {
+            max = Math.max(max, nums[i]);
+
+            if (max - suffMin[i] <= k) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
